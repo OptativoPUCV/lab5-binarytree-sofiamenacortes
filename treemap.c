0c0d42870113d2d596121905fd198925560574c7
Your Name <you@example.com>
2024-05-11 21:54:45 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -48,7 +48,8 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) { //inserta un nuevo nodo en el arbol
-    TreeNode* newNode = createTreeNode(key, value); //si inserto un nuevo par clave-valor se necesita un nuevo nodo
+    //si inserto un nuevo par clave-valor se necesita un nuevo nodo
+    TreeNode* newNode = createTreeNode(key, value); 
     if(newNode == NULL){
         return;
     }
